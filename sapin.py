12b0e488da74586a33687d6e2e4563ef6dbedebb
Nils <72611176+Nils92@users.noreply.github.com>
2020-12-18 10:12:10 +0100

    Add files via upload

---
+++ sapin.py
@@ -0,0 +1,15 @@
+etoile="*"
+nombre= input('Donne moi un chiffre bogosse: ')
+nombre=int(nombre) * 2 + 1
+if nombre>11:
+    print("     /",etoile * (nombre-10),"I")
+if nombre>9:
+    print("    /",etoile * (nombre-8),"I")
+if nombre>7:
+    print("   /",etoile * (nombre-6),"I")
+if nombre>5:
+    print("  /",etoile * (nombre-4),"I")
+if nombre>3:
+    print(" /",etoile * (nombre-2),"I")
+print("/",etoile * nombre,"I")
+print()

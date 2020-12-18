27b7b0c23e756c23971e3f81f6e1cc0a1f8cd2b4
Nils <72611176+Nils92@users.noreply.github.com>
2020-12-18 10:28:43 +0100

    Update sapin.py

--- sapin.py
+++ sapin.py
@@ -1,15 +1,20 @@
 etoile="*"
 nombre= input('Donne moi un chiffre bogosse: ')
 nombre=int(nombre) * 2 
+espace=" "
+
 if nombre>11:
-    print("     /",etoile * (nombre-10),"I")
+    print("      /",etoile * (nombre-12),"\\")
+    print("     /",etoile * (nombre-10),"\\")
 if nombre>9:
-    print("    /",etoile * (nombre-8),"I")
+    print("    /",etoile * (nombre-8),"\\")
 if nombre>7:
-    print("   /",etoile * (nombre-6),"I")
+    print("   /",etoile * (nombre-6),"\\")
 if nombre>5:
-    print("  /",etoile * (nombre-4),"I")
+    print("  /",etoile * (nombre-4),"\\")
 if nombre>3:
-    print(" /",etoile * (nombre-2),"I")
-print("/",etoile * nombre,"I")
-print()
+    print(" /",etoile * (nombre-2),"\\")
+print("/",etoile * nombre,"\\")
+print(espace * (nombre-5),"#")
+
+
